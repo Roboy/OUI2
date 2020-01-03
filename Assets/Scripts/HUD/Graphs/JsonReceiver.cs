60394b92f6e473f1e8f811910c21f50b34027bdd
Janik Müller <janik.mueller@tum.de>
2020-01-03 10:33:24 +0100

    Imported scripts to control the graphs and receive input

---
+++ Assets/Scripts/HUD/Graphs/JsonReceiver.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JsonReceiver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ReceivedJsonData d = new ReceivedJsonData(0, "temperature", BitConverter.GetBytes(27), 0);
+        //print(JsonUtility.ToJson(d));
+        //string s = Resources.Load<TextAsset>("TestTemplate").text;
+        TextAsset[] textAssets = Resources.LoadAll<TextAsset>("JsonTemplates");
+        foreach (TextAsset asset in textAssets)
+        {
+            print(asset.text);
+            ReceivedJsonData r = JsonUtility.FromJson<ReceivedJsonData>(asset.text);
+            if (r == null)
+            {
+                print("Json " + asset.text);
+                continue;
+            }
+            print(r.pos);
+            
+            // Create a new Bytearray
+            Byte[] bs = BitConverter.GetBytes(2);
+            Byte[] bs2 = BitConverter.GetBytes(13);
+            Byte[] bs3 = new byte[8];
+            Array.Copy(bs, 0, bs3, 0, 4);
+            Array.Copy(bs2, 0, bs3, 4, 4);
+            
+            // Convert Bytearray to two ints
+            int msg_type = BitConverter.ToInt32(bs3, 0);
+            print(msg_type);
+            // 
+            if (msg_type == (int)LiveDataType.Graph)
+            {
+                int y = BitConverter.ToInt32(bs3, 4);
+                print(y);
+            }
+
+        }
+        //print(s);
+        
+        
+    }
+}
+
+enum LiveDataType
+{
+    ErrorMsg, Msg, Graph
+}
---
+++ Assets/Scripts/HUD/Graphs/ReceivedJsonData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ReceivedJsonData
+{
+    public int template_ID;
+    public string type;
+    public byte[] data;
+    public int pos;
+    
+    // determines how many labels should be shown on the x axis of the graph
+    public int numLabelsShownX;
+    // determines how many labels should be shown on the y axis of the graph
+    public int numLabelsShownY;
+    // the color of the graph
+    public int color;
+
+    public ReceivedJsonData(int templateId, string type, byte[] data, int pos)
+    {
+        template_ID = templateId;
+        this.type = type;
+        this.data = data;
+        this.pos = pos;
+    }
+}

79b282acb686b323d2eb2e626482acf56128b0c3
Leenert <leenert.specht@tum.de>
2020-01-07 15:36:27 +0100

    Re-Added Transition, Implemented Interface, Applied Prefabs

---
+++ Assets/Scripts/Managers/TransitionManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class TransitionManager : MonoBehaviour
+{
+    PostProcessVolume postProcessVolume;
+    
+    [SerializeField]
+    private float overallDuration = 1.5f;
+    [SerializeField]
+    private bool isLerping = false;
+
+    private int curDirection = 1;
+
+    private float lerpingValue;
+    private float startValue = 0.0f, endValue = 1.0f;
+    private float curTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.postProcessVolume = GetComponent<PostProcessVolume>();
+    }
+
+    void Update()
+    {
+        if (isLerping)
+        {
+            curTime += Time.deltaTime;
+
+            float flow = curTime / overallDuration;
+
+            if (flow < 1)
+            {
+                lerpingValue = startValue + (endValue - startValue) * flow;
+                this.postProcessVolume.weight = lerpingValue;
+            }
+            else
+            {
+                lerpingValue = startValue;
+                float tmp = this.startValue;
+                this.startValue = this.endValue;
+                this.endValue = tmp;
+                this.curTime = 0;
+
+                if (curDirection == 1)
+                {
+                    curDirection = -1;
+                } else if (curDirection == -1)
+                {
+                    curDirection = 1;
+                    isLerping = false;
+                }
+            }
+        }
+    }
+
+    public void StartTransition()
+    {
+        this.isLerping = true;
+    }
+}

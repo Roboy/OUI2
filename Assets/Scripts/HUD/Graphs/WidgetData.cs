1275857c82e181694797a026590d13e5bf1f390c
Philip Neugebauer <>
2020-01-28 22:21:24 +0100

    Widgets can now be created and their pos set to the log

---
+++ Assets/Scripts/HUD/Graphs/WidgetData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WidgetData : Singleton<WidgetData>
+{
+    public GameObject[] positionsUI;
+    public GameObject[] positionsDetailedPanels;
+    public GameObject[] positionsText;
+    public GameObject[] positionsAdvancedMenu;
+}

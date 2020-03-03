898fed3eded4f7aedec034f5b5869b378dcd4636
Janik Müller <janik.mueller@tum.de>
2020-03-03 12:55:45 +0100

    setup construct scene
    sense gloves, menu, buzz manager

---
+++ Assets/ConstructManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ConstructManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.AltGr))
+        {
+            AdditiveSceneManager asm = GameObject.FindGameObjectWithTag("AdditiveSceneManager").GetComponent<AdditiveSceneManager>();
+            //asm.UnloadScene(null,null);
+            StartCoroutine(asm.LoadScene(Scenes.CONSTRUCT, DelegateBeforeConstructLoad, DelegateAfterConstructLoad));
+        }
+    }
+
+    void DelegateAfterConstructLoad()
+    {
+        Transform cameraOrigin = GameObject.FindGameObjectWithTag("CameraOrigin").transform;
+        Transform leftSenseGlove = GameObject.FindGameObjectWithTag("SenseGloveLeft").transform;
+        leftSenseGlove.SetParent(cameraOrigin, false);
+        Transform openMenuButtonPressurePlate = leftSenseGlove.GetChild(1).GetChild(0);
+        openMenuButtonPressurePlate.GetComponent<SpringJoint>().connectedAnchor = openMenuButtonPressurePlate.position;
+        leftSenseGlove.GetComponent<SteamVR_TrackedObject>().enabled = true;
+        GameObject.FindGameObjectWithTag("SenseGloveRight").transform.SetParent(cameraOrigin, false);
+        GameObject.FindGameObjectWithTag("ConstructObjects").transform.GetChild(0).SetParent(cameraOrigin, false);
+    }
+
+    void DelegateBeforeConstructLoad()
+    {
+
+    }
+}

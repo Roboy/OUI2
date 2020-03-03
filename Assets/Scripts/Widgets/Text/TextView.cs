1ac1475a20377520b12d20f6ed23e24877818ff4
Max <max.heimbrock@gmx.de>
2020-03-03 16:11:55 +0100

    WIP

---
+++ Assets/Scripts/Widgets/Text/TextView.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Widgets
+{
+    public class TextView : View
+    {
+        public override void HideView()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void ShowView()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void ChangeMessage()
+        {
+
+        }
+    }
+}
--- Assets/Scripts/Widgets/Text/TextWidget.cs
+++ Assets/Scripts/Widgets/Text/TextWidget.cs
@@ -1,5 +1,64 @@
 ﻿using UnityEngine;
 
-public class TextWidget : MonoBehaviour
+namespace Widgets
 {
+    public class TextWidget : Widget
+    {
+        TextView view;
+
+        public Color color;
+        public int fontSize;
+
+        public string displayedMessage;
+
+        public override View GetView()
+        {
+            return view;
+        }
+
+        public override void ProcessRosMessage(RosJsonMessage rosMessage)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public new void Init(RosJsonMessage rosJsonMessage)
+        {            
+            color = WidgetUtility.BytesToColor(rosJsonMessage.textColor);
+            fontSize = rosJsonMessage.textFontSize;
+            
+            base.Init(rosJsonMessage);
+        }
+
+        private void changeDisplayedMessage(string messageToDisplay, Color textColor, int textFontSize)
+        {
+            if (messageToDisplay.Equals(""))
+            {
+                return;
+            }
+
+            if (textColor == null)
+            {
+                textColor = color;
+            }
+            
+            if (textFontSize == 0)
+            {
+                textFontSize = fontSize;
+            }
+
+            displayedMessage = messageToDisplay;
+
+            view.ChangeMessage();
+        }
+
+        public override void RestoreViews(GameObject viewParent)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void UpdateInClass()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
 }

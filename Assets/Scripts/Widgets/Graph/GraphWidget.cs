1feb7036e35866a1ec824ff002fecc4345e89cda
Philip <philngbr@googlemail.com>
2020-03-04 14:51:55 +0100

    Livedata now uses the time stamp from the ros message

--- Assets/Scripts/Widgets/Graph/GraphWidget.cs
+++ Assets/Scripts/Widgets/Graph/GraphWidget.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Widgets
@@ -12,12 +13,13 @@ namespace Widgets
         public int numXLabels = 2;
         public int numYLabels = 3;
 
-        public List<float> datapoints;
-        public float[] datapointsArray;
+
+        public List<Datapoint> datapoints;
+        public Datapoint[] datapointsArray;
         
         public void Awake()
         {
-            datapoints = new List<float>();
+            datapoints = new List<Datapoint>();
         }
 
         public new void Init(RosJsonMessage context)
@@ -35,17 +37,23 @@ namespace Widgets
             {
                 color = WidgetUtility.BytesToColor(rosMessage.graphColor);
             }
+            DateTime dt = DateTime.Now;
+            if (rosMessage.graphDatapointTime != 0)
+            {
+                System.DateTime epochStart = new System.DateTime(1970, 1, 1, 0, 0, 0, System.DateTimeKind.Utc);
+                dt = epochStart.AddMilliseconds(rosMessage.graphDatapointTime);
+            }
             if (rosMessage.graphDatapoint != 0)
             {
-                AddDatapoint(rosMessage.graphDatapoint);
+                AddDatapoint(new Datapoint(dt, rosMessage.graphDatapoint));
             }
         }
 
-        public void AddDatapoint(float newDatapoint)
+        public void AddDatapoint(Datapoint newDatapoint)
         {
             if (datapoints.Count == SIZE)
             {
-                datapoints.Remove(0);
+                datapoints.RemoveAt(0);
             }
 
             datapoints.Add(newDatapoint);
@@ -70,8 +78,14 @@ namespace Widgets
 
         public struct Datapoint
         {
-            float time;
-            float data;
+            public DateTime time;
+            public float data;
+
+            public Datapoint(DateTime time, float newDatapoint)
+            {
+                this.time = time;
+                this.data = newDatapoint;
+            }
         }
     }
 }

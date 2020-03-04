8bc2b27dd92b30d38c0cb3457431dd52f32a19c9
Janik Müller <janik.mueller@tum.de>
2020-03-04 17:05:18 +0100

    update StateManager

--- Assets/Scripts/Managers/StateManager.cs
+++ Assets/Scripts/Managers/StateManager.cs
@@ -4,38 +4,98 @@ using UnityEngine.SceneManagement;
 using Valve.VR;
 
 public class StateManager : Singleton<StateManager> {
-    private StateMachine stateMachine;
+    //private StateMachine stateMachine;
+    private States currentState;
 
-    delegate void MakeTransition();
+    delegate void MakeTransition(); // What is this for?
 
     AdditiveSceneManager additiveSceneManager;
     TransitionManager transitionManager;
+
+    public enum States
+    {
+        HUD, Construct
+    }
 
     // Start is called before the first frame update
     void Start() {
         additiveSceneManager = GameObject.FindGameObjectWithTag("AdditiveSceneManager").GetComponent<AdditiveSceneManager>();
         transitionManager = GameObject.FindGameObjectWithTag("TransitionManager").GetComponent<TransitionManager>();
 
-        stateMachine = new StateMachine(new HUDState(this));
+        currentState = States.HUD;
+        //stateMachine = new StateMachine(new HUDState(this));
     }
 
     public void GoToNextState() {
-        stateMachine.GoToNextState();
+        switch (currentState)
+        {
+            case States.HUD:
+                additiveSceneManager.ChangeScene(Scenes.CONSTRUCT, null, null, null, DelegateAfterConstructLoad);
+                currentState = States.Construct;
+                break;
+            case States.Construct:
+                additiveSceneManager.ChangeScene(Scenes.HUD, null, DelegateOnConstructUnload, null, null);
+                currentState = States.HUD;
+                break;
+            default:
+                Debug.LogWarning("Unhandled State: Please specify the next State after " + currentState);
+                break;
+        }
+        //stateMachine.GoToNextState();
     }
 
     void CheckForDebugInput() {
         // Go to next state when space is pressed
         if (Input.GetKeyDown(KeyCode.Space))
-            stateMachine.GoToNextState();
+            GoToNextState();
     }
 
     // Update is called once per frame
     void Update() {
         CheckForDebugInput();
-
-        stateMachine.Update();
     }
 
+    #region Delegates
+    void DelegateAfterConstructLoad()
+    {
+        Transform cameraOrigin = GameObject.FindGameObjectWithTag("CameraOrigin").transform;
+        Transform leftSenseGlove = GameObject.FindGameObjectWithTag("SenseGloveLeft").transform;
+        leftSenseGlove.SetParent(cameraOrigin, false);
+        leftSenseGlove.GetChild(1).GetComponent<ShowOpenMenuButton>().CompareObject = cameraOrigin;
+        leftSenseGlove.GetComponent<SteamVR_TrackedObject>().enabled = true;
+        GameObject.FindGameObjectWithTag("SenseGloveRight").transform.SetParent(cameraOrigin, false);
+
+        GameObject constructObjects = GameObject.FindGameObjectWithTag("ConstructObjects");
+        GameObject roboy = GameObject.FindGameObjectWithTag("Roboy");
+
+        roboy.transform.position = cameraOrigin.position + new Vector3(0f, 1.5f, 0f);
+        roboy.transform.rotation = Quaternion.Euler(roboy.transform.rotation.eulerAngles + cameraOrigin.rotation.eulerAngles);
+
+        GameObject.FindGameObjectWithTag("SubMenu3D").transform.SetParent(cameraOrigin, false);
+        /*Transform constructObjects = GameObject.FindGameObjectWithTag("ConstructObjects").transform;
+        Transform roboy = constructObjects.GetChild(0);
+        roboy.position = cameraOrigin.position + new Vector3(0f, 1.5f, 0f);
+        roboy.rotation = Quaternion.Euler(roboy.rotation.eulerAngles + cameraOrigin.rotation.eulerAngles);
+        constructObjects.GetChild(1).SetParent(cameraOrigin, false);*/
+    }
+
+    void DelegateOnConstructUnload()
+    {
+        Destroy(GameObject.FindGameObjectWithTag("SenseGloveLeft"));
+        Destroy(GameObject.FindGameObjectWithTag("SenseGloveRight"));
+        Transform cameraOrigin = GameObject.FindGameObjectWithTag("CameraOrigin").transform;
+        for(int i = 0; i < cameraOrigin.childCount; i++)
+        {
+            if (cameraOrigin.GetChild(i).CompareTag("SubMenu3D"))
+            {
+                Destroy(cameraOrigin.GetChild(i).gameObject);
+            }
+        }
+    }
+    #endregion
+
+
+    /*
     #region STATES
 
     private class HUDState : IState {
@@ -81,26 +141,7 @@ public class StateManager : Singleton<StateManager> {
             return new HUDState(owner);
         }
 
-        void DelegateAfterConstructLoad() {
-            Transform cameraOrigin = GameObject.FindGameObjectWithTag("CameraOrigin").transform;
-            Transform leftSenseGlove = GameObject.FindGameObjectWithTag("SenseGloveLeft").transform;
-            leftSenseGlove.SetParent(cameraOrigin, false);
-            leftSenseGlove.GetChild(1).GetComponent<ShowOpenMenuButton>().CompareObject = cameraOrigin;
-            leftSenseGlove.GetComponent<SteamVR_TrackedObject>().enabled = true;
-            GameObject.FindGameObjectWithTag("SenseGloveRight").transform.SetParent(cameraOrigin, false);
 
-            GameObject constructObjects = GameObject.FindGameObjectWithTag("ConstructObjects");
-            GameObject roboy = GameObject.FindGameObjectWithTag("Roboy");
-            
-            roboy.transform.position = cameraOrigin.position + new Vector3(0f, 1.5f, 0f);
-            roboy.transform.rotation = Quaternion.Euler(roboy.transform.rotation.eulerAngles + cameraOrigin.rotation.eulerAngles);
-            
-            GameObject.FindGameObjectWithTag("SubMenu3D").transform.SetParent(cameraOrigin, false);
-            /*Transform constructObjects = GameObject.FindGameObjectWithTag("ConstructObjects").transform;
-            Transform roboy = constructObjects.GetChild(0);
-            roboy.position = cameraOrigin.position + new Vector3(0f, 1.5f, 0f);
-            roboy.rotation = Quaternion.Euler(roboy.rotation.eulerAngles + cameraOrigin.rotation.eulerAngles);
-            constructObjects.GetChild(1).SetParent(cameraOrigin, false);*/
         }
     }
 
@@ -145,4 +186,5 @@ public class StateManager : Singleton<StateManager> {
     }
 
     #endregion
+    */
 }

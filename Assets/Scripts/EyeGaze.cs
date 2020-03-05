bc8d702c1b2e5dbe17773f76af4c13e5e7383683
Janik Müller <janik.mueller@tum.de>
2020-03-05 11:04:46 +0100

    Integrate Eye Tracking SDKs, scripts and prefab

---
+++ Assets/Scripts/EyeGaze.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tobii.G2OM;
+
+public class EyeGaze : MonoBehaviour, IGazeFocusable
+{
+    IconStateManager ism;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ism = this.transform.parent.GetComponent<IconStateManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GazeFocusChanged(bool hasFocus)
+    {
+        //ism.Focus(hasFocus);
+        glow(hasFocus);
+    }
+
+    public void glow(bool glow)
+    {
+        if (glow)
+        {
+            ism.ShowNotification();
+        } else
+        {
+            ism.StopNotification();
+        }
+    }
+}
---
+++ Assets/Scripts/ScaleToParentSize.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleToParentSize : MonoBehaviour
+{
+    //Should default to true
+    public bool HasFixedScale;
+    RectTransform parent;
+    Vector2 size;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent = this.transform.parent.GetComponent<RectTransform>();
+        Rescale();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!HasFixedScale)
+        {
+            if(parent.sizeDelta != size)
+            {
+                Rescale();
+            }
+        }
+    }
+
+    private void Rescale()
+    {
+        size = parent.sizeDelta;
+        this.transform.localScale = new Vector3(size.x, size.y, this.transform.localScale.z);
+    }
+}

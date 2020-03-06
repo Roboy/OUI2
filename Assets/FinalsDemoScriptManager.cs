0a12b45a59816381a7efc389134134b85eff6a07
Janik Müller <janik.mueller@tum.de>
2020-03-06 17:40:42 +0100

    implement random cube pos for trash

--- Assets/FinalsDemoScriptManager.cs
+++ Assets/FinalsDemoScriptManager.cs
@@ -7,10 +7,14 @@ public class FinalsDemoScriptManager : MonoBehaviour
     public GameObject TrashBin;
     public GameObject TrashCube;
     public int NumberOfCubes;
+
+    private RoomArea room;
+
     GameObject trashBin;
     // Start is called before the first frame update
     void Start()
     {
+        room = GameObject.FindGameObjectWithTag("CameraOrigin").GetComponent<RoomArea>();
         if(NumberOfCubes <= 0)
         {
             NumberOfCubes = 10;
@@ -52,6 +56,12 @@ public class FinalsDemoScriptManager : MonoBehaviour
 
     Vector3 GetRandomPositionInRoom()
     {
-        return Vector3.zero;
+        float y = Random.Range(0.1f, 1.7f);
+        Vector3 result = new Vector3(Random.Range(-4.5f, 34.5f), y, Random.Range(-44.5f, 4.5f));
+        while (!room.validatePositionInRoom(result))
+        {
+            result = new Vector3(Random.Range(-4.5f, 34.5f), y, Random.Range(-44.5f, 4.5f));
+        }
+        return result;
     }
 }

79d9385c86b1dfdd3ed78a38b5bf8539badec6ed
Janik Müller <janik.mueller@tum.de>
2020-03-06 14:28:44 +0100

    Quest prefabs and logic

--- Assets/FinalsDemoScriptManager.cs
+++ Assets/FinalsDemoScriptManager.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class FinalsDemoScriptManager : MonoBehaviour
 {
+    public GameObject TrashBin;
+    public GameObject TrashCube;
+    public int NumberOfCubes;
+    GameObject trashBin;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(NumberOfCubes <= 0)
+        {
+            NumberOfCubes = 10;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartQuest();
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            StopQuest();
+        }
+    }
+
+    public void StartQuest()
+    {
+        GameObject roboy = GameObject.FindGameObjectWithTag("Roboy");
+        trashBin = Instantiate(TrashBin, roboy.transform);
+        for(int i = 0; i < NumberOfCubes; i++)
+        {
+            Instantiate(TrashCube, GetRandomPositionInRoom(), Quaternion.identity);
+        }
+    }
+
+    public void StopQuest()
+    {
+        GameObject[] allTrashCubes = GameObject.FindGameObjectsWithTag("Trash");
+        foreach (GameObject trash in allTrashCubes)
+        {
+            Destroy(trash);
+        }
+        Destroy(trashBin);
+    }
+
+    Vector3 GetRandomPositionInRoom()
+    {
+        return Vector3.zero;
     }
 }
---
+++ Assets/TrashBin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashBin : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Trash") && !other.GetComponent<SenseGlove_Grabable>().IsInteracting())
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}

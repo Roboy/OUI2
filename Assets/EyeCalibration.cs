0d3036a9bc62c01f930dd4431bf52747ff101324
Max <max.heimbrock@gmx.de>
2020-03-09 13:41:40 +0100

    Add Calibrate Eyes button to menu

---
+++ Assets/EyeCalibration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+using ViveSR.anipal.Eye;
+
+public class EyeCalibration : MonoBehaviour
+{
+    public void Execute()
+    {
+        SRanipal_Eye_API.LaunchEyeCalibration(IntPtr.Zero);
+    }
+}
